d338a131d50a5b0e1f98dc2c1e2ee277db203bb6
EpicDeveloper <vgdobon91@gmail.com>
2021-01-25 16:02:47 +0100

    Actualizado 25/01/2021 16:02

---
+++ ComercioDigital/ComercioDigital/DTOs/Producto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComercioDigital.DTOs
+{
+    class Producto
+    {
+        public string Marca { get; set; }
+        public string Nombre { get; set; }
+        public float Precio { get; set; }
+        public Vendedor vendedor { get; set; }
+        public string Descripcion { get; set;}
+        public int Valoracion { get; set; }
+        public List<int> Valoraciones { get; set; }
+        public static int Increment { get; set; }
+        public int IdProducto { get; set; }
+        private DateTime FechaPuestaVenta { get; set; }
+        public string CodigoDescuento { get; set; }
+
+        public Producto(string marca, string nombre, float precio, Vendedor vendedor, string descripcion, DateTime fechaPuestaVenta, string codigoDescuento)
+        {
+            Marca = marca;
+            Nombre = nombre;
+            Precio = precio;
+            this.vendedor = vendedor;
+            Descripcion = descripcion;
+            FechaPuestaVenta = fechaPuestaVenta;
+            CodigoDescuento = codigoDescuento;
+        }
+    }
+}
---
+++ ComercioDigital/ComercioDigital/DTOs/Vendedor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComercioDigital.DTOs
+{
+    public class Vendedor
+    {
+        public static int Incrementer { get; set; }
+        public int IdVendedor { get; set; }
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+        public int Valoracion { get; set; }
+
+        public List<int> Valoraciones;
+
+        public Vendedor(string nombre, string ciudad)
+        {
+            Nombre = nombre ?? throw new ArgumentNullException(nameof(nombre));
+            Ciudad = ciudad ?? throw new ArgumentNullException(nameof(ciudad));
+            Valoracion =  Valoraciones.Sum() / Valoraciones.Count();
+            Incrementer++;
+            IdVendedor = Incrementer;
+
+
+        }
+    }
+}
